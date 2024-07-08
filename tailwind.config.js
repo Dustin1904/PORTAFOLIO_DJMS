367ef053a587a2a511707adf9ca89d6270ff6dbf
Dustin <dustin04ms@gmail.com>
2024-07-08 08:27:59 -0500

    Primer Avance

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-black":"#111111"
+      }
+    },
+  },
+  plugins: [],
+}
+
